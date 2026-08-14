02ffbdc3f23d6c6ca47dc3b5a2b07814acc7f32e
Almhusm02 <hmmakrami2@ngrid.sa>
2026-08-14 14:59:45 +0300

    Publish config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+/* لا تضع أسراراً أو كلمات مرور هنا. يضبط بعد تسجيل تطبيق Microsoft Entra. */
+window.SOA_CONFIG = Object.freeze({
+  dataProvider: 'local', // local | sharepoint
+  tenantId: '',
+  clientId: '',
+  siteId: '',
+  activitiesListId: '',
+  ncrListId: '',
+  workbookShareUrl: '', // لا يُنشر رابط ملف الشركة في النسخة العامة
+  excelTableName: 'TaskTracker'
+});
